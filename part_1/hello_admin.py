0ed321d0383f355fb577d656fe0278126ad5f7df
Tung <tunga3109@gmail.com>
2022-02-27 03:54:34 +0300

    commit

--- part_1/hello_admin.py
+++ part_1/hello_admin.py
@@ -24,4 +24,11 @@ for digit in digits:
     else:
         print(f'{digit}th')
 
+current_users = ['Anton', 'Katya', 'Lena', 'Edgar', 'Tonya']
+new_users = ['Katya', 'ANTON', 'Andrew', 'Vlad', 'Vasya']
 
+for new_user in new_users:
+    if new_user in current_users:
+        print(f'{new_user} is already exist')
+    else:
+        print(f'{new_user} has never been used')
