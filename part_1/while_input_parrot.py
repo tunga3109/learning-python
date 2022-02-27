52bc43faa6c3e1308a4cff1e0c551a6428dba55d
Tung <tunga3109@gmail.com>
2022-02-28 01:14:28 +0300

    rename

---
+++ part_1/while_input_parrot.py
@@ -0,0 +1,10 @@
+message = input("Tell me something, and I will repeat it back to you: ")
+print(message)
+
+prompt = "\nTell me something, and I will repeat it back to you:" 
+prompt += "\nEnter 'quit' to end the program. "
+message = ""
+while message != 'quit': # пока мы не напишем quit, программа не выйдет
+    message = input(prompt)
+    print(message)
+
