60a76586a18fb1181c6fed8021229710411625bb
Tung <tunga3109@gmail.com>
2022-02-27 03:08:56 +0300

    commit

--- part_1/hello_admin.py
+++ part_1/hello_admin.py
@@ -4,3 +4,10 @@ for user in users:
         print(f'Hello {user}, would you like to see a status report?')
     else:
         print(f'Hello {user}, thank you for logging in again')
+
+us = []
+if us:
+    for user in users: 
+        print(f'{user} has been added') # если есть элементы в списке
+else:
+    print('empty list') # если нет элементов в списке
