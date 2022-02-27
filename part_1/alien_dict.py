d0767bfec10cdb4255afc776ddef93668d7bb47d
Tung <tunga3109@gmail.com>
2022-02-27 06:17:28 +0300

    dict

--- part_1/alien_dict.py
+++ part_1/alien_dict.py
@@ -1,4 +1,54 @@
-alien_0 = {'color': 'green', 'points': 5}
+from turtle import speed
 
-print(alien_0['color'])
+
+alien_0 = {'color': 'green', 'points': 5, 'speed': 'medium'} # словарь
+
+print(alien_0['color']) #получить значение через ключ
 print(alien_0['points'])
+
+new_points = alien_0['points'] # получить значение через переменную
+print(f"You just earned {new_points} points!")
+
+alien_0['x_position'] = 0 # добавление ключ-значение
+alien_0['y_position'] = 25 # добавление ключ-значение
+print(alien_0)
+
+print(f"The alien is {alien_0['color']}.")
+alien_0['color'] = 'yellow'
+print(f"The alien is now {alien_0['color']}.")
+
+if alien_0['speed'] == 'slow':
+    x_increment = 1
+elif alien_0['speed'] == 'medium':
+    x_increment = 2
+else:
+    # Пришелец двигается быстро.
+    x_increment = 3
+
+alien_0['x_position'] = alien_0['x_position'] + x_increment
+print(f"New position: {alien_0['x_position']}")
+
+del alien_0['points'] # Удаление пар «ключ-значение»
+print(alien_0)
+
+favorite_languages = {
+    'jen': 'python',
+    'sarah': 'c',
+    'edward': 'ruby',
+    'phil': 'python',
+    }
+
+language = favorite_languages['sarah'].title() 
+print(f"Sarah's favorite language is {language}.")
+
+alien_0 = {'color': 'green', 'speed': 'slow'}
+
+point_value = alien_0.get('points', 'No point value assigned.') # get()
+print(point_value)
+
+alien_0 = {
+    'color': 'green', 
+    'points': 5, 
+    'speed': 'medium'} 
+
+print(f'{ alien_0["color"]}\n{alien_0["points"]}\n{alien_0["speed"]}')
