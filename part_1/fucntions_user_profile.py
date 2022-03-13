5935b1916e2cae8d9a52d4ad8863dc4b11bfb016
Tung <tunga3109@gmail.com>
2022-03-13 22:41:38 +0400

    func

---
+++ part_1/fucntions_user_profile.py
@@ -0,0 +1,10 @@
+def build_profile(first, last, **user_info):
+    """Строит словарь с информацией о пользователе."""
+    user_info['first_name'] = first
+    user_info['last_name'] = last
+    return user_info
+    
+user_profile = build_profile('albert', 'einstein',
+                             location='princeton',
+                             field='physics')
+print(user_profile)
--- part_1/functions_pizza.py
+++ part_1/functions_pizza.py
@@ -8,3 +8,24 @@ make_pizza('mushrooms', 'green peppers', 'extra cheese')
 
 #Звездочка в имени параметра *toppings приказывает Python создать пустой кортеж с именем toppings и упаковать в него все полученные значения. 
 #Результат команды print в теле функции показывает, что Python успешно справляется и с вызовом функции с одним значением, и с вызовом с тремя значениями.
+
+
+def make_pizza(*toppings):
+    """Выводит описание пиццы."""
+    print("\nMaking a pizza with the following toppings:")
+    for topping in toppings:
+        print(f"- {topping}")
+
+make_pizza('pepperoni')
+make_pizza('mushrooms', 'green peppers', 'extra cheese')
+
+#Позиционные аргументы с произвольными наборами аргументов
+
+
+def make_pizza(size, *toppings):
+    """Выводит описание пиццы."""
+    print(f"\nMaking a {size}-inch pizza with the following toppings:")
+    for topping in toppings:
+        print(f"- {topping}")
+make_pizza(16, 'pepperoni')
+make_pizza(12, 'mushrooms', 'green peppers', 'extra cheese')
