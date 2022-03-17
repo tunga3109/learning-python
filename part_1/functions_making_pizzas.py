dad830d8b70d95a27293ac6f12d8b01b0fbcc4df
Tung <tunga3109@gmail.com>
2022-03-17 15:14:09 +0400

    funtions import

---
+++ part_1/functions_making_pizzas.py
@@ -0,0 +1,5 @@
+import functions_pizza
+
+functions_pizza.make_pizza(15,'beef','tomato')
+
+
--- part_1/functions_pizza.py
+++ part_1/functions_pizza.py
@@ -1,23 +1,23 @@
 #Передача произвольного набора аргументов
 
-def make_pizza(*toppings):
-    """Вывод списка заказанных топпингов."""
-    print(toppings)
-make_pizza('pepperoni')
-make_pizza('mushrooms', 'green peppers', 'extra cheese')
+#def make_pizza(*toppings):
+#    """Вывод списка заказанных топпингов."""
+#    print(toppings)
+#make_pizza('pepperoni')
+#make_pizza('mushrooms', 'green peppers', 'extra cheese')
 
 #Звездочка в имени параметра *toppings приказывает Python создать пустой кортеж с именем toppings и упаковать в него все полученные значения. 
 #Результат команды print в теле функции показывает, что Python успешно справляется и с вызовом функции с одним значением, и с вызовом с тремя значениями.
 
 
-def make_pizza(*toppings):
-    """Выводит описание пиццы."""
-    print("\nMaking a pizza with the following toppings:")
-    for topping in toppings:
-        print(f"- {topping}")
-
-make_pizza('pepperoni')
-make_pizza('mushrooms', 'green peppers', 'extra cheese')
+#def make_pizza(*toppings):
+#    """Выводит описание пиццы."""
+#    print("\nMaking a pizza with the following toppings:")
+#    for topping in toppings:
+#        print(f"- {topping}")
+#
+#make_pizza('pepperoni')
+#make_pizza('mushrooms', 'green peppers', 'extra cheese')
 
 #Позиционные аргументы с произвольными наборами аргументов
 
@@ -27,5 +27,6 @@ def make_pizza(size, *toppings):
     print(f"\nMaking a {size}-inch pizza with the following toppings:")
     for topping in toppings:
         print(f"- {topping}")
-make_pizza(16, 'pepperoni')
-make_pizza(12, 'mushrooms', 'green peppers', 'extra cheese')
+
+#make_pizza(16, 'pepperoni')
+#make_pizza(12, 'mushrooms', 'green peppers', 'extra cheese')
