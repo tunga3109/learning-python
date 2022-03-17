d077532b820f3deac83113b27551c7c27af92fbc
Tung <tunga3109@gmail.com>
2022-03-17 17:32:27 +0400

    class

--- part_1/part_1_6_class/lesson_1_class_dog.py
+++ part_1/part_1_6_class/lesson_1_class_dog.py
@@ -15,3 +15,12 @@ class Dog():
     def roll_over(self):
         print(f'{self.name} rolled over!')
 
+my_dog = Dog('willie', 6) # my_dog - это экземпляр класса
+
+# Обращение к атрибутам без скобок ()
+print(f"My dog's name is {my_dog.name}.") 
+print(f"My dog is {my_dog.age} years old.")
+
+# Вызов методов cо скобками ()
+my_dog.sit() 
+my_dog.roll_over()
