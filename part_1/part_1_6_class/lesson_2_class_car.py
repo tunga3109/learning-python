7f3c3e607d52b61724adbc7ec2a419e734c8b77a
Tung <tunga3109@gmail.com>
2022-03-28 01:57:41 +0400

    class car

--- part_1/part_1_6_class/lesson_2_class_car.py
+++ part_1/part_1_6_class/lesson_2_class_car.py
@@ -4,11 +4,26 @@ class Car():
         self.make = make
         self.model = model
         self.year = year
+        self.odometer_reading = 0
 
     def get_descriptive_name(self):
         long_name = f'{self.year} {self.make} {self.model}'
         return long_name.title()
+    
+    def read_odometer(self):
+        """Выводит пробег машины в милях."""
+        print(f"This car has {self.odometer_reading} miles on it.")
+
+    def update_odometer(self, mileage):
+        """Устанавливает на одометре заданное значение.
+        При попытке обратной подкрутки изменение отклоняется."""
+        if mileage >= self.odometer_reading:
+            self.odometer_reading = mileage
+        else:
+            print("You can't roll back an odometer!")
 
 
 my_new_car = Car('audi', 'a4', 2019)
 print(my_new_car.get_descriptive_name())
+my_new_car.update_odometer(23)
+my_new_car.read_odometer()
