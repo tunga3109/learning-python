2080d0f3507b9114ed2295b1f8b98fcb92c0d4bc
Tung <tunga3109@gmail.com>
2022-04-02 03:09:01 +0400

    class

--- part_1/part_1_6_class/lesson_4_class_cube.py
+++ part_1/part_1_6_class/lesson_4_class_cube.py
@@ -1,4 +1,7 @@
-from random import randint
+#1
+
+
+from random import choice, randint
 
 class Die():
 
@@ -8,7 +11,31 @@ class Die():
     def roll_die(self):
         print(randint(1,self.sides))
 
-c = Die(20)
-for i in range(10):
-    c.roll_die()
+#c = Die(20)
+#for i in range(10):
+#    c.roll_die()
+#
+#class Loterey():
+#
+#    def __init__(self, *combination):
+#        self.combination = ['1','2','3','4','5','6','7','8','9','a','b','c','d','e']
+#        self.winner = []
+#
+#    def random(self):
+#        for i in range(4):
+#            a = choice(self.combination)
+#            self.winner.append(a)
+#
+#        b = ''.join(self.winner)
+#        if b == 'abc4':
+#            print('You are winner')
+#        else:
+#            print(f'Try again \n comb {b}')
+#
+#    
+#  
+#d = Loterey()
+
+
+        
         
